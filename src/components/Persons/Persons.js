302471c50bca3a53456a37ad24f2a152bfbbf909
DagmawiAbate <dagmawiabate2@gmail.com>
2022-10-20 16:41:33 +0300

    Using shouldComponetUpdate for optimization

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -14,7 +14,12 @@ class Persons extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[Person.js] shouldComponentUpdate')
-    return true
+    if(nextProps.persons !== this.props.persons) {
+      return true
+    } else {
+      return false
+    }
+    // return true
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
--- src/containers/App.js
+++ src/containers/App.js
@@ -40,7 +40,7 @@ class App extends Component {
   }
 
   componentDidUpdate() {
-    console.log('[App.js] componentDidUpdate');
+    console.log('[App.js] componentDidUpdate')
   }
 
   nameChangedHandler = (event, id) => {
@@ -88,13 +88,21 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <button onClick={() => {this.setState({showCockpit: false})}}>Remove Cockpit</button>
-        {this.state.showCockpit ? <Cockpit
-          title={this.props.appTitle}
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler}
-        /> : null}
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false })
+          }}
+        >
+          Remove Cockpit
+        </button>
+        {this.state.showCockpit ? (
+          <Cockpit
+            title={this.props.appTitle}
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler}
+          />
+        ) : null}
         {persons}
       </div>
     )
