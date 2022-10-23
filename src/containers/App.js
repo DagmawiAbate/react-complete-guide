a13cfabf2a9c7a72818fedaabe301a0cccd13f58
DagmawiAbate <dagmawiabate2@gmail.com>
2022-10-23 13:22:05 +0300

    Higher Order Components (HOC)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,80 +1,83 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import classes from './App.css'
-import Persons from '../components/Persons/Persons'
-import Cockpit from '../components/Cockpit/Cockpit'
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    console.log('[App.js] constructor')
+    super(props);
+    console.log('[App.js] constructor');
   }
 
   state = {
     persons: [
-      { id: 'sddsds', name: 'Dagi', age: 24 },
-      { id: 'asdf1', name: 'Muluhabet', age: 23 },
-      { id: 'sasdw', name: 'Kate', age: 21 }
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false,
     showCockpit: true
-  }
+  };
 
   static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateProps', props)
-    return state
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
   }
 
   // componentWillMount() {
-  //   console.log('[App.js] componentWillMount')
+  //   console.log('[App.js] componentWillMount');
   // }
 
   componentDidMount() {
-    console.log('[App.js] componentDidMount')
+    console.log('[App.js] componentDidMount');
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('[App.js] shouldComponentUpdate')
-    return true
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
   }
 
   componentDidUpdate() {
-    console.log('[App.js] componentDidUpdate')
+    console.log('[App.js] componentDidUpdate');
   }
 
   nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex((p) => {
-      return p.id === id
-    })
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
 
-    const person = { ...this.state.persons[personIndex] }
+    const person = {
+      ...this.state.persons[personIndex]
+    };
 
-    // const person = Object.assign({}, this.state.persons[personIndex])
+    // const person = Object.assign({}, this.state.persons[personIndex]);
 
-    person.name = event.target.value
+    person.name = event.target.value;
 
-    const persons = [...this.state.persons]
-    persons[personIndex] = person
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
 
-    this.setState({ persons: persons })
-  }
+    this.setState({ persons: persons });
+  };
 
-  deletePersonHandler = (personIndex) => {
+  deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
-    const persons = [...this.state.persons]
-    persons.splice(personIndex, 1)
-    this.setState({ persons: persons })
-  }
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons
-    this.setState({ showPersons: !doesShow })
-  }
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
-    console.log('[App.js] render')
-    let persons = null
+    console.log('[App.js] render');
+    let persons = null;
 
     if (this.state.showPersons) {
       persons = (
@@ -83,14 +86,14 @@ class App extends Component {
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
         />
-      )
+      );
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button
           onClick={() => {
-            this.setState({ showCockpit: false })
+            this.setState({ showCockpit: false });
           }}
         >
           Remove Cockpit
@@ -104,10 +107,10 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </div>
-    )
+      </WithClass>
+    );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App
+export default App;
