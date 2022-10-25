cf624ead7b804c8db2a30f1cccfec188eed2cae1
DagmawiAbate <dagmawiabate2@gmail.com>
2022-10-25 12:31:50 +0300

    Setting state correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import classes from './App.css';
-import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit';
-import withClass from '../hoc/withClass';
+import classes from './App.css'
+import Persons from '../components/Persons/Persons'
+import Cockpit from '../components/Cockpit/Cockpit'
+import withClass from '../hoc/withClass'
 import Aux from '../hoc/Auxiliary'
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    console.log('[App.js] constructor');
+    super(props)
+    console.log('[App.js] constructor')
   }
 
   state = {
@@ -20,12 +20,13 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
-  };
+    showCockpit: true,
+    changeCounter: 0
+  }
 
   static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateFromProps', props);
-    return state;
+    console.log('[App.js] getDerivedStateFromProps', props)
+    return state
   }
 
   // componentWillMount() {
@@ -33,52 +34,57 @@ class App extends Component {
   // }
 
   componentDidMount() {
-    console.log('[App.js] componentDidMount');
+    console.log('[App.js] componentDidMount')
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('[App.js] shouldComponentUpdate');
-    return true;
+    console.log('[App.js] shouldComponentUpdate')
+    return true
   }
 
   componentDidUpdate() {
-    console.log('[App.js] componentDidUpdate');
+    console.log('[App.js] componentDidUpdate')
   }
 
   nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
-    });
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id
+    })
 
     const person = {
       ...this.state.persons[personIndex]
-    };
+    }
 
     // const person = Object.assign({}, this.state.persons[personIndex]);
 
-    person.name = event.target.value;
+    person.name = event.target.value
 
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
 
-    this.setState({ persons: persons });
-  };
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      }
+    })
+  }
 
-  deletePersonHandler = personIndex => {
+  deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({ persons: persons });
-  };
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({ persons: persons })
+  }
 
   togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  };
+    const doesShow = this.state.showPersons
+    this.setState({ showPersons: !doesShow })
+  }
 
   render() {
-    console.log('[App.js] render');
-    let persons = null;
+    console.log('[App.js] render')
+    let persons = null
 
     if (this.state.showPersons) {
       persons = (
@@ -87,14 +93,14 @@ class App extends Component {
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
         />
-      );
+      )
     }
 
     return (
       <Aux>
         <button
           onClick={() => {
-            this.setState({ showCockpit: false });
+            this.setState({ showCockpit: false })
           }}
         >
           Remove Cockpit
@@ -109,9 +115,9 @@ class App extends Component {
         ) : null}
         {persons}
       </Aux>
-    );
+    )
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default withClass(App, classes.App);
+export default withClass(App, classes.App)
